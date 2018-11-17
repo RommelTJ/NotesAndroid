91dbf5464366eda9199cee75e0f5479237fdbdcc
Rommel Rico <me@rommelrico.com>
2018-11-16 20:16:40 -0800

    Adding NoteEditorActivity

---
+++ app/src/main/java/com/rommelrico/notesandroid/NoteEditorActivity.kt
@@ -0,0 +1,12 @@
+package com.rommelrico.notesandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteEditorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_editor)
+    }
+}

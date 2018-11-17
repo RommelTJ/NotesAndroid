71cb56ab0810a3548e61397a9b2a58170cd06556
Rommel Rico <me@rommelrico.com>
2018-11-16 20:08:07 -0800

    Grabbing the shared preferences

--- app/src/main/java/com/rommelrico/notesandroid/MainActivity.kt
+++ app/src/main/java/com/rommelrico/notesandroid/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.rommelrico.notesandroid
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.ListView
 import java.util.ArrayList
+import java.util.HashSet
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +18,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedPreferences = applicationContext.getSharedPreferences("com.rommelrico.notesandroid", Context.MODE_PRIVATE)
+        val listView = findViewById<ListView>(R.id.listView)
+
+        val set = sharedPreferences?.getStringSet("notes", null) as HashSet<String>
+
+        if (set == null) {
+            notes.add("Example Note")
+        } else {
+            notes = ArrayList(set)
+        }
     }
 }

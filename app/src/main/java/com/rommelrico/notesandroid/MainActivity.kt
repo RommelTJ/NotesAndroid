9cd4a6353c91ed87176078f78f4b08b04090463a
Rommel Rico <me@rommelrico.com>
2018-11-16 20:06:44 -0800

    Internal MainActivity properties

--- app/src/main/java/com/rommelrico/notesandroid/MainActivity.kt
+++ app/src/main/java/com/rommelrico/notesandroid/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.rommelrico.notesandroid
 
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    internal var notes = ArrayList<String>()
+    internal var arrayAdapter: ArrayAdapter<*>? = null
+    internal var sharedPreferences: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

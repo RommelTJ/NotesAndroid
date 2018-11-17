7c6a35f09d54c3271c037980739dadb89881f5f2
Rommel Rico <me@rommelrico.com>
2018-11-16 20:18:39 -0800

    Note ID

--- app/src/main/java/com/rommelrico/notesandroid/NoteEditorActivity.kt
+++ app/src/main/java/com/rommelrico/notesandroid/NoteEditorActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class NoteEditorActivity : AppCompatActivity() {
 
+    internal var noteId: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_editor)
